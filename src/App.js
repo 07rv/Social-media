92ba45b4d9e47f0fe821b2afea418df9bb62b3af
Rohit verma <28vrohit@gmail.com>
2022-08-27 17:31:14 +0530

    start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import { YouTube } from "@mui/icons-material";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar"; 
+import { Box, Stack } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <YouTube/>
-      </header>
-    </div>
+    <Box>
+      <Navbar/>
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+        <Sidebar/>
+        <Feed/>
+        <Rightbar/>
+      </Stack>
+    </Box>
   );
 }
 
---
+++ src/components/Rightbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Box } from '@mui/material'
+
+const Rightbar = () => {
+  return (
+    <Box sx={{display:{xs:"none", sm:"block"}}}>
+      Rightbar
+    </Box>
+  )
+}
+
+export default Rightbar
